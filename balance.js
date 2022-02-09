ac38781a4306d912ac48e5552b5d6bf9fb30fccc
Isaac Presley <76850372+ipresley96@users.noreply.github.com>
2022-02-09 18:26:48 -0500

    Add files via upload

---
+++ balance.js
@@ -0,0 +1,37 @@
+function Balance(){
+  const [show, setShow]         = React.useState(true);
+  const [balance, setBalance]     = React.useState('');
+  const ctx = React.useContext(UserContext);  
+
+
+
+function updateBalance(){
+  if (!validate(balance, 'password')) return;
+  ctx.users.push({balance:100});
+  setShow(false);
+}    
+
+  return (
+    <Card
+      bgcolor="primary"
+      header="Balance"
+      status={balance}
+      body={show ? (  
+              <>
+              Balance : `${balance}`
+              <br />
+              <br />
+              Deposit 
+              <br />
+              <input type="input" className="form-control" id="name" placeholder="Enter value" value={balance} onChange={e => setName(e.currentTarget.value)} /><br/>
+              <button type="submit" className="btn btn-light" onClick={updateBalance}>Submit</button>
+              </>
+            ):(
+              <>
+              <h5>Success</h5>
+              <button type="submit" className="btn btn-light" onClick={clearForm}>Add another account</button>
+              </>
+            )}
+    />
+  )
+}
